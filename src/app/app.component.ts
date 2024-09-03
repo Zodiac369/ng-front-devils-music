b70386435ea016a4cd535bc4d3560f1f8f278530
Zodiac369 <emre.ogk@hotmail.fr>
2024-09-03 19:33:03 +0200

    Nav Icons & Links

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fontAwesomeIcons } from '../assets/shared/font-awesome-icons';
+import { NavigationComponent } from "./layout/navigation/navigation.component";
+import { LibraryComponent } from "./layout/library/library.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FontAwesomeModule, NavigationComponent, LibraryComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'ng-front-devils-music';
+export class AppComponent implements OnInit {
+  title = 'Devil\'s Music 🧟';
+
+  private faIconLibrary : FaIconLibrary = inject(FaIconLibrary);
+
+  ngOnInit(): void {
+    this.initFontAwesome();
+  }
+
+  private initFontAwesome() : void {
+    this.faIconLibrary.addIcons(...fontAwesomeIcons);
+  }
 }
+
+
+
+
